9908363f295d30fcba75f22f2da5c6ca481e1c94
Alex C-G <alexcg@outlook.com>
2020-07-09 11:15:00 +1200

    fix(app): imports

---
+++ model_list.py
@@ -0,0 +1,10 @@
+model_dict = {
+    "Star Trek script": "alexcg1/trekbot", 
+    "Movie script": "cpierse/gpt2_film_scripts",
+    "IMDB review": "lvwerra/gpt2-imdb-pos",
+    "Cover letter": "jonasmue/cover-letter-gpt2",
+    "------": None,
+    "General text (distilgpt2)": "distilgpt2",
+    "General text (GPT-2)": "gpt2-medium",
+    "General text (GPT)": "openai-gpt",
+}
